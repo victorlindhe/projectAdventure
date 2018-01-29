22e81a6ed05f52d86461c72e03baccc0896737ee
Victor Lindhé <victorlindhe@gmail.com>
2018-01-29 13:13:35 +0100

    Simple game loop added

--- game/main.js
+++ game/main.js
@@ -1,5 +1,23 @@
-$(document).ready(function () {
+// Initial file for the game code
+$(document).ready(() => {
 
-  console.log('Starting game...');
+  const GAME_LOOP_INTERVAL = 200;
+  const ESC_BUTTON_KEY_CODE = 27;
+
+  let gameLoop = function () {
+    console.log('Tick...');
+  }
+
+  // Starting the game...
+  console.log('Starting Project Adventure...');
+  let interval = setInterval(gameLoop, GAME_LOOP_INTERVAL);
+
+  // Stopping the game when pressing escape
+  $(document).keyup((e) => {
+    if(e.keyCode === ESC_BUTTON_KEY_CODE) {
+      clearInterval(interval);
+      alert('Game stopped!');
+    }
+  });
 
 });
